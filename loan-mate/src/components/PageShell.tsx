a7e43a06e8723895ece9d9e010ad22476ac9b792
Ju-Min-Kyung <alsrud7767@naver.com>
2025-11-24 15:14:45 +0900

    feat: pageShell 정의

---
+++ loan-mate/src/components/PageShell.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import type { ReactNode } from "react";
+import NavigationBar from "@/components/NavigationBar";
+
+type PageShellProps = {
+  title?: string;
+  showBack?: boolean;
+  children: ReactNode;
+};
+
+export default function PageShell({
+  title,
+  showBack = true,
+  children,
+}: PageShellProps) {
+  return (
+    <div className="flex min-h-full flex-col">
+      <NavigationBar title={title} showBack={showBack} />
+      <main className="flex-1 pb-12 pt-4">{children}</main>
+    </div>
+  );
+}
