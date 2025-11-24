1ffe9e0364189c6d4e317a425a5f155d75d96553
Ju-Min-Kyung <alsrud7767@naver.com>
2025-11-24 19:42:06 +0900

    feat: 스크롤 안되게 수정

--- loan-mate/src/components/PageShell.tsx
+++ loan-mate/src/components/PageShell.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import type { ReactNode } from "react";
 import NavigationBar from "@/components/NavigationBar";
+import type { ReactNode } from "react";
 
 type PageShellProps = {
   title?: string;
@@ -9,15 +9,11 @@ type PageShellProps = {
   children: ReactNode;
 };
 
-export default function PageShell({
-  title,
-  showBack = true,
-  children,
-}: PageShellProps) {
+export default function PageShell({ title, showBack = true, children }: PageShellProps) {
   return (
-    <div className="flex min-h-full flex-col">
+    <div className="flex min-h-screen flex-col bg-white">
       <NavigationBar title={title} showBack={showBack} />
-      <main className="flex-1 pb-12 pt-4">{children}</main>
+      <main className="flex-1 px-6 py-6">{children}</main>
     </div>
   );
 }
