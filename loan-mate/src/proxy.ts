b98e60f8c0e69a1976e2bf8ed6deb226c4101281
Ju-Min-Kyung <alsrud7767@naver.com>
2025-11-25 17:04:59 +0900

    chore: middleware deprecated 되어 proxy로 변경

--- loan-mate/src/proxy.ts
+++ loan-mate/src/proxy.ts
@@ -1,13 +1,12 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const accessToken = req.cookies.get("accessToken")?.value;
   const pathname = req.nextUrl.pathname;
 
   const isLoginPage = pathname.startsWith("/login");
   const isProtectedPage = pathname === "/";   
-
   // 로그인된 상태에서 /login 접근 금지
   if (accessToken && isLoginPage) {
     return NextResponse.redirect(new URL("/", req.url));
