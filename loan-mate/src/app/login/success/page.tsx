a173fc70211599b4c01cbf301c7cb766d3b7c8b4
Ju-Min-Kyung <alsrud7767@naver.com>
2025-11-25 16:15:14 +0900

    chore: 주석 형태 수정

--- loan-mate/src/app/login/success/page.tsx
+++ loan-mate/src/app/login/success/page.tsx
@@ -10,6 +10,6 @@ export default function LoginSuccessPage() {
     router.replace("/");
   }, [router]);
 
-  {/* TODO: 추후 로딩 컴포넌트로 바꿔야 함 */}
+  // TODO: 추후 로딩 컴포넌트로 바꿔야 함 
   return <p>로그인 중...</p>;
 }
