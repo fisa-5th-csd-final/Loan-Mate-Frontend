6c209b6fda9c7d0680ae4b2d4345fe37f32584fb
hyebim <shb010424@gmail.com>
2025-11-26 18:22:47 +0900

    [FIX] vercel 오류 해결

--- loan-mate/src/app/auto-deposit/prepaid/layout.tsx
+++ loan-mate/src/app/auto-deposit/prepaid/layout.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 
 import NavigationBar from "@/components/navigation/BackRouteNavigation";
 import { NavigationProvider, useNavigation } from "@/components/navigation/NavigationContext";
