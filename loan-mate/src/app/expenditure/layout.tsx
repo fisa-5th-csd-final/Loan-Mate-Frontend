985dee1d98e215e8bc15483c038fdb396c584583
Ju-Min-Kyung <alsrud7767@naver.com>
2025-11-26 11:56:14 +0900

    design: expenditure 공통 레이아웃 정의

---
+++ loan-mate/src/app/expenditure/layout.tsx
@@ -0,0 +1,16 @@
+import type { ReactNode } from "react";
+import NavigationBar from "@/components/navigation/BackRouteNavigation";
+
+export default function ExpenditureLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex flex-col h-screen bg-white">
+      <NavigationBar title="나의 지출 한도 정하기" />
+
+      <main className="flex-1 overflow-auto px-4">
+        {children}
+      </main>
+    </div>
+  );
+}
+
+
