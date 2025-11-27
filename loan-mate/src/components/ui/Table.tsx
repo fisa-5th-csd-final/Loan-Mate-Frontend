ef37b964e0f95a49975dfcaffe4bb2deb6fdac8c
Ju-Min-Kyung <alsrud7767@naver.com>
2025-11-27 10:32:56 +0900

    design: 공통으로 사용되는 테이블 컴포넌트 정의

---
+++ loan-mate/src/components/ui/Table.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React from "react";
+
+interface BaseProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export function Table({ children, className }: BaseProps) {
+  return (
+    <div className={`w-full flex flex-col ${className ?? ""}`}>
+      {children}
+    </div>
+  );
+}
+
+export function TableHeader({children, className, columns}: BaseProps & { columns?: string }) {
+  return (
+    <div
+      className={`grid items-center text-[13px] px-1 text-gray-700 font-semibold mb-2 ${className ?? ""
+        }`}
+      style={{ gridTemplateColumns: columns || "1fr 1fr 1fr" }}
+    >
+      {children}
+    </div>
+  );
+}
+
+
+export function TableRow({ children, className, columns }: BaseProps & { columns?: string }) {
+  return (
+    <div
+      className={`grid items-center py-3 px-1 ${className ?? ""}`}
+      style={{ gridTemplateColumns: columns || "1fr 1fr 1fr" }}
+    >
+      {children}
+    </div>
+  );
+}
+
+
+export function TableCell({ children, className }: BaseProps) {
+  return (
+    <div className={`text-gray-800 text-sm ${className ?? ""}`}>
+      {children}
+    </div>
+  );
+}
+
+export function TableDivider() {
+  return <div className="h-[1px] bg-white my-1" />;
+}
